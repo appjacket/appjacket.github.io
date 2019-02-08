9869a9072e79f0dcd486a306c545b361eb0a8197
DaveThomson-Appjacket <45829844+DaveThomson-Appjacket@users.noreply.github.com>
2019-02-08 02:13:45 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 window.addEventListener('load', function() {
-  
-  var authEngine = new Auth();
+
   var authBtn = document.getElementById('btn-login'); 
+  var authEngine = new Auth(authBtn);
   
   authBtn.addEventListener('click', function(e) {
     e.preventDefault();
-    if(this.text == "Login"){
+    if(authBtn.text == "Login"){
       authEngine.webAuth.authorize();
     }else {
      authEngine.logout(); 
